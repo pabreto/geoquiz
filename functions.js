8d3232926bb4251be2794d078492263b5591e1bd
pabreto <pabretonniere@hotmail.fr>
2019-04-28 17:55:28 +0200

    Load Map

--- functions.js
+++ functions.js
@@ -47,30 +47,29 @@ function random_letter(id){
 }
 
 function initialize() {
-		     geocoder = new google.maps.Geocoder();
-		     var latlng = new google.maps.LatLng(-34.397, 150.644);
-		     var mapOptions = {
-		       zoom: 8,
-		       center: latlng
-		     }
-		     map = new google.maps.Map(document.getElementById('map'), mapOptions);
- }
-
-function codeAddress() {
-   var address = document.getElementById('address').value;
-		     geocoder.geocode( { 'address': address}, function(results, status) {
-		       if (status == 'OK') {
-		         map.setCenter(results[0].geometry.location);
-		         var marker = new google.maps.Marker({
-		             map: map,
-		             position: results[0].geometry.location
-		         });
-		       } else {
-		         alert('Geocode was not successful for the following reason: ' + status);
-		       }
-		     });
+    var prop = {
+    center:new google.maps.LatLng(0,0),
+    zoom:1,
+    };
+    var map=new google.maps.Map(document.getElementById("map"), prop);
 }
 
+function codeAddress() {
+  var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == 'OK') {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
+}
+//google.maps.event.addDomListener(window, 'load', initialize);
+var geocoder;
 
 	//	    function get_distance(ville1, ville2) {
 	//			dlon = lon2 - lon1
